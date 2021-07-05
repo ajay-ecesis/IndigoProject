17f630f01f86e614a8e1e10d0711e09f644aa2e4
aCm <aCm@LAPTOP-JLKPKL5Q>
2021-07-05 21:14:10 +0530

    Plan Banner slider updated

--- pagecomponents/PlanBanner.js
+++ pagecomponents/PlanBanner.js
@@ -1,8 +1,25 @@
-import { urlFor } from '../utils/tools'
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { urlFor } from "../utils/tools"
 import BlockContent from '@sanity/block-content-to-react';
 
 
 const PlanBanner = ({content}) => {
+    
+    const settings = { 
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        accessibility: false,
+        infinite: true,
+        cssEase: 'linear',
+        prevArrow: '<span class="prev"><i class="fal fa-long-arrow-alt-left"></i></span>',
+                nextArrow: '<span class="next"><i class="fal fa-long-arrow-alt-right"></i></span>',
+        dots: true,
+        arrows: false
+      };
 
     const overrides = {
         h2: props => <h2 className="heading-inner" {...props} />,
@@ -25,21 +42,28 @@ const PlanBanner = ({content}) => {
 
     return (
         <section className="section trustBrand projektIndigo">
-              <div className="container-fluid ">
-                  <div className="row">
-                      <div className="wrapper col-12">
-                          <div className="row">
-                              <div className="col-md-12 col">
-                                  {content?.image && <img src={urlFor(content?.image)} className="projekt-img"  alt="" />}
-                                  <div className="content">
-                                     {content?.description && <BlockContent blocks={content?.description} serializers={serializers} />}
-                                  </div>
-                              </div>
-                          </div>
-                      </div>       
-                  </div>
-              </div>
-          </section>  
+        <div className="container-fluid ">
+            <div className="row">
+                <div className="wrapper col-12">
+                    <div className="row project_indigo_slider">
+                    <Slider {...settings}>
+                        {content.map((item,i)=>(
+                             <div key={i} className="col-md-12 col">
+                             <img src={urlFor(item?.image)} className="projekt-img"  alt="" />
+                             <div className="content">
+                                {item.description && <BlockContent blocks={item?.description} serializers={serializers} />}
+                             </div>
+                         </div>
+                        ))}
+                         
+                        </Slider>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+        </section>
+
     )
 }
 
